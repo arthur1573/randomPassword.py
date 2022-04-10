a12411c0d796ebbd32663d81335d735945da2f04
tzlmzyt <arthursimpsons@outlook.com>
2022-04-10 23:13:18 +0800

    repeat letter

--- randomPassword16.py
+++ randomPassword16.py
@@ -1,7 +1,7 @@
 import random
 import clipboard
-length = 16
 all = "abcdefghjkmnpkrstuvwxyz" + "ABCDEFGHJKMNPKRSTUVWXYZ" + "23456789" + "@#$%^*_" # delete: i l o, I L O, 1 0, !()-=+
-sample = random.sample(all, length)
-password = "".join(sample)
-clipboard.copy(password)
+password = random.choices(all, k=16)
+password2 = "".join(password)
+clipboard.copy(password2)
+print(password2)
