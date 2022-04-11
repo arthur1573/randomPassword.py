ff296b3969f2dc2ae4ebdda3161e52196760e11f
tzlmzyt <arthursimpsons@outlook.com>
2022-04-11 10:12:44 +0800

    password letter can repeat

--- randomPassword16.py
+++ randomPassword16.py
@@ -1,6 +1,6 @@
 import random
 import clipboard
-all = "abcdefghjkmnpkrstuvwxyz" + "ABCDEFGHJKMNPKRSTUVWXYZ" + "23456789" + "@#$%^*_" # delete: i l o, I L O, 1 0, !()-=+
+all = "abcdefghjkmnpkrstuvwxyz" + "ABCDEFGHJKMNPKRSTUVWXYZ" + "23456789" + "@#$%^*_" # delete: i l o, I L O, 1 0, !()-=+[]\;',./
 password = random.choices(all, k=16)
 password2 = "".join(password)
 clipboard.copy(password2)
