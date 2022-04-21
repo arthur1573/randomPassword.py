868326ff04c05d18c144fb1b3d8ca7c33e5b57c1
tzlmzyt <arthursimpsons@outlook.com>
2022-04-21 13:32:01 +0800

    removed module: clipboard

--- randomPassword16.py
+++ randomPassword16.py
@@ -1,7 +1,5 @@
 import random
-import clipboard
 all = "abcdefghjkmnpkrstuvwxyz" + "ABCDEFGHJKMNPKRSTUVWXYZ" + "23456789" + "@#$%^*_" # delete: i l o, I L O, 1 0, !()-=+[]\;',./
 password = random.choices(all, k=16)
 password2 = "".join(password)
-clipboard.copy(password2)
 print(password2)
