41b785ab7fda6b4b0cdce7c945e0831832b736d0
tzlmzyt <arthursimpsons@outlook.com>
2022-06-05 02:32:02 +0800

    update

--- randomPassword16.py
+++ randomPassword16.py
@@ -1,10 +1,18 @@
 import random
-import pyperclip
 
+
+# combine all letter, number, signes.
 all = "abcdefghjkmnpkrstuvwxyz" + "ABCDEFGHJKMNPKRSTUVWXYZ" + "23456789" + "@#$%^*_" # delete: i l o, I L O, 1 0, !()-=+[]\;',./
 password = random.choices(all, k=16)
-password2 = "".join(password)
-pyperclip.copy(password2)
 
-print(password2)
-print('The text to be copied to the clipboard.')
+# convert list to string
+password2 = "".join(password)
+
+
+try:
+    import pyperclip
+    pyperclip.copy(password2)
+    print('The text to be copied to the clipboard.')
+except:
+    print(password2)
+    
